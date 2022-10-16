e3f4db4d202221d15c303e18e032841b2439e3f6
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-10-16 20:16:48 -0300

    Reestruturação do projeto
    - Remoção das pastas API e frontend, o projeto será somente frontend
    - Exclusão das dependências instaladas anteriormente
    - Exclusão do gitignore
    - Criação da estrutura base do HTML
    - Começando a implementar a feature de ler o excel e gerar o XML

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const btnConvert = document.getElementById('convert')
+const inputFile = document.getElementById('excel-file')
+const inputDestinationFile = document.getElementById('destination-file')
+const inputStartRow = document.getElementById('start-row')
+const inputEndRow = document.getElementById('end-row')
+const inputName = document.getElementById('name')
+const inputSummary = document.getElementById('summary')
+const inputPreconditions = document.getElementById('preconditions')
+const button = document.getElementById('download')
+const linkDownload = document.getElementById('link-download')
+
+btnConvert.addEventListener('click', () => {
+    if (!inputFile.files[0]) {
+        alert('Selecione um arquivo do Excel!')
+        return;
+    }
+
+    if (hasFieldEmpty()) {
+        alert('Preencha todos os campos!')
+        return;
+    }
+
+    const outputFilename = inputDestinationFile.value
+    const startRow = inputStartRow.value - 1
+    const endRow = inputEndRow.value - 1
+    const columnName = inputName.value - 1
+
+    const testcases = xmlbuilder2.create().ele('testcases')
+    readXlsxFile(inputFile.files[0]).then((rows) => {
+        for (let currentRow = startRow; currentRow <= endRow; currentRow++) {
+            const testcase = testcases.ele('testcase')
+            testcase.att('internalid', "")
+            testcase.att('name', rows[currentRow][columnName])
+        }
+
+        const xml = testcases.end({ prettyPrint: true })
+        const file = new File([xml], outputFilename, {
+            type: 'text/plain'
+        })
+        updateDownloadButton(file)
+    })
+})
+
+function hasFieldEmpty() {
+    return (
+        inputDestinationFile.value == "" ||
+        inputStartRow.value == "" ||
+        inputEndRow.value == "" ||
+        inputName.value == "" ||
+        inputSummary.value == "" ||
+        inputPreconditions.value == ""
+    )
+}
+
+function updateDownloadButton(file) {
+    linkDownload.href = window.URL.createObjectURL(file)
+    linkDownload.download = file.name || 'saida.xml'
+    button.classList.remove('hidden')
+}
